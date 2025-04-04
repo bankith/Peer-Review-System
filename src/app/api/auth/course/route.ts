944aba57d680ad97b54a9a6e4a4a50223bfa09fa
Tanakan Sookgasi <bankstanakan@gmail.com>
2025-04-05 00:00:38 +0700

    add: course api

--- src/app/api/auth/course/route.ts
+++ src/app/api/auth/course/route.ts
@@ -3,24 +3,31 @@ import { User, UserRoleEnum } from "@/entities/User";
 import { ResponseFactory } from '@/utils/ResponseFactory';
 import { AppDataSource, initializeDataSource } from '@/data-source';
 import { CourseEnrollment } from '@/entities/CourseEnrollment';
+import { verifyToken } from '@/utils/verifyToken';
+import { headers } from 'next/headers';
+import { Course } from '@/entities/Course';
 
 export async function GET(req: NextRequest) {
   try {    
+      const authorization = (await headers()).get('authorization')
+      var jwt = verifyToken(authorization!);
+      if(jwt == null){
+        return NextResponse.json(ResponseFactory.error("Unauthorize access", 'Unauthorize'), {status: 401});
+      }
+
       const idParam = req?.nextUrl?.searchParams.get('studentId')      
       if(idParam != null){
         const id = parseInt(idParam!);
         await initializeDataSource();
 
-        const categoriesWithQuestions = await AppDataSource
-        .getRepository(CourseEnrollment)
-        .createQueryBuilder("CourseEnrollment")
-        .leftJoinAndSelect("CourseEnrollment.student", "student")
-        .leftJoinAndSelect("CourseEnrollment.course", "course")
-        .where("CourseEnrollment.studentId = :id", { id: id })
+        var courses = await AppDataSource
+        .getRepository(Course)
+        .createQueryBuilder("Course")
+        .leftJoinAndSelect("Course.courseEnrollments", "courseEnrollment")        
+        .where("courseEnrollment.studentId = :id", { id: jwt.id })        
         .getMany()
 
-
-        return NextResponse.json(ResponseFactory.success(categoriesWithQuestions),{status: 201});
+        return NextResponse.json(ResponseFactory.success(courses),{status: 200});
       }
 
       return NextResponse.json(ResponseFactory.success("1"),{status: 201});
