b75685c8fee9afe45580ab7011c7fd66d6f1e0bb
Tanakan Sookgasi <bankstanakan@gmail.com>
2025-04-12 12:46:50 +0700

    add: otp table

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,18 +1,11 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany, BaseEntity, CreateDateColumn, UpdateDateColumn, OneToOne, JoinColumn, ManyToMany, JoinTable, type Relation } from 'typeorm';
 import { InstructorProfile } from './InstructorProfile';
 import { StudentProfile } from './StudentProfile';
-import { Assignment } from './Assignment';
 import { AssignmentSubmission } from './AssignmentSubmission';
-import { PeerReview } from './PeerReview';
 import { PeerReviewSubmission } from './PeerReviewSubmission';
-import { PeerReviewComment } from './PeerReviewComment';
-import { AssignmentGrading } from './AssignmentGrading';
-import { PeerReviewGrading } from './PeerReviewGrading';
-import { Course } from './Course';
 import { CourseEnrollment } from './CourseEnrollment';
 import { CourseInstructor } from './CourseInstructor';
-import { GroupMember } from './GroupMember';
-import { StudentGroup } from './StudentGroup';
+import { Otp } from './Otp';
 
 export enum UserRoleEnum {
     student = 1,
@@ -48,17 +41,8 @@ export class User {
     @OneToMany(() => CourseInstructor, (c) => c.instructor, {eager: true,})
     courseInstructors: Relation<CourseInstructor[]>;
 
-    // @OneToMany(() => StudentGroup, (c) => c.groupMembers, {cascade: true, eager: true})    
-    // groupMembers: Relation<GroupMember[]>;
-
     @OneToMany(() => AssignmentSubmission, d => d.user, {eager: true,})
     assignments: Relation<AssignmentSubmission[]>;
-
-    // @OneToMany(() => AssignmentSubmission, assignmentSubmission => assignmentSubmission.user)
-    // assignmentSubmissions: AssignmentSubmission[];
-
-    // @OneToMany(() => PeerReview, peerReview => peerReview.reviewer)
-    // peerReviews: PeerReview[];
 
     @OneToMany(() => PeerReviewSubmission, peerReviewSubmission => peerReviewSubmission.reviewer)
     reviewerPeerReviewSubmissions: PeerReviewSubmission[];
@@ -66,14 +50,8 @@ export class User {
     @OneToMany(() => PeerReviewSubmission, peerReviewSubmission => peerReviewSubmission.reviewee)
     revieweePeerReviewSubmissions: PeerReviewSubmission[];
 
-    // @OneToMany(() => PeerReviewComment, peerReviewComment => peerReviewComment.user)
-    // peerReviewComments: PeerReviewComment[];
-
-    // @OneToMany(() => AssignmentGrading, assignmentGrading => assignmentGrading.gradedBy)
-    // assignmentGradings: AssignmentGrading[];
-
-    // @OneToMany(() => PeerReviewGrading, peerReviewGrading => peerReviewGrading.gradedBy)
-    // peerReviewGradings: PeerReviewGrading[];
+    @OneToMany(() => Otp, otp => otp.user)
+    otps: Promise<Otp[]>;
 
     @CreateDateColumn()
     createdDate: Date;
